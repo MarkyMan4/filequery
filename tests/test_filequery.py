d705d57d2dcaa678362ead1d33aa4952ac5c67d0
MarkyMan4 <markus.hutnik@gmail.com>
2023-02-27 19:15:22 -0600

    added unit tests

--- tests/test_filequery.py
+++ tests/test_filequery.py
@@ -11,16 +11,47 @@ sample_data_path = os.path.join(os.getcwd(), 'example')
 sys.path.append(sample_data_path)
 
 from filequery.filedb import FileDb
+from filequery.queryresult import QueryResult
 
 class TestFileQuery(unittest.TestCase):
-    def test_exec_query(self):
-        fdb = FileDb('example/test.csv')
-        res = fdb.exec_query('select * from test')
-        
+    def check_select_star_from_test(self, res: QueryResult):
         self.assertEqual(len(res.records), 3)
 
         for rec in res.records:
             self.assertEqual(len(rec), 3)
+
+    def test_exec_query(self):
+        fdb = FileDb('example/test.csv')
+        res = fdb.exec_query('select * from test')
+        
+        self.check_select_star_from_test(res)
+
+    def test_filesdir_no_trailing_slash(self):
+        fdb = FileDb('example/data')
+        res = fdb.exec_query('select * from test')
+        
+        self.check_select_star_from_test(res)
+
+    def test_filesdir_trailing_slash(self):
+        fdb = FileDb('example/data/')
+        res = fdb.exec_query('select * from test')
+        
+        self.check_select_star_from_test(res)
+
+    def test_multi_query(self):
+        fdb = FileDb('example/data/')
+        res = fdb.exec_many_queries([
+            'select * from test;',
+            'select * from test1;',
+            """
+            select *
+                from test t1
+                inner join test1 t2
+                on t1.col1 = t2.col1;
+            """
+        ])
+
+        self.assertEqual(len(res), 3)
 
 if __name__ == '__main__':
     unittest.main()
