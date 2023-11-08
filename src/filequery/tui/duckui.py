86911e26085ec84da00ee161b911742c1a81095d
MarkyMan4 <markus.hutnik@gmail.com>
2023-11-07 22:45:38 -0600

    in TUI, only execute query at cursor and highlight the query that was executed

--- src/filequery/tui/duckui.py
+++ src/filequery/tui/duckui.py
@@ -1,8 +1,13 @@
+# TODO add option to save queries and result to files
+
+from typing import Tuple
+
 import duckdb
 from textual.app import App, ComposeResult
 from textual.binding import Binding
 from textual.containers import Horizontal, Vertical
 from textual.widgets import DataTable, Footer, Markdown, Static, TextArea
+from textual.widgets.text_area import Selection
 
 from .help_content import help_md
 
@@ -59,27 +64,96 @@ class DuckUI(App):
     def action_toggle_help(self):
         self.help_box.visible = not self.help_box.visible
 
-    def action_execute_query(self):
-        # TODO only execute query at the cursor, a nice touch would be to highlight the query that ran
+    def _display_error_in_table(self, error_msg: str):
+        """
+        Displays an error message in the result table
 
+        :param error_msg: error message to display
+        :type error: str
+        """
+        self.result_table.clear(columns=True)
+        self.result_table.add_column("error")
+        self.result_table.add_row(error_msg)
+
+    def _find_query_at_cursor(
+        self, cursor_x: int, cursor_y: int
+    ) -> Tuple[str, Selection]:
+        """
+        Find the query at cursor_x, cursor_y
+
+        :param cursor_x: line cursor is on
+        :type cursor_x: int
+        :param cursor_y: column cursor is on
+        :type cursor_y: int
+        :return: tuple with the query selection and the span of the query given as a Selection which
+                 can be used to highlight the query
+        :rtype: Tuple[str, Selection]
+        """
         queries = self.text_area.text.split(";")
+
+        line = 0
+        col = 0
+        query_idx_to_run = -1
+        found_query = False
+        selection_start = (0, 0)
+        selection_end = (0, 0)
+
+        for i, query in enumerate(queries):
+            selection_start = (line, col)
+
+            # whether we've seen any characters in the current query besides newline and space
+            found_non_empty_chars = False
+
+            for c in query:
+                if line == cursor_x and col == cursor_y:
+                    query_idx_to_run = i
+                    found_query = True
+
+                if c == "\n":
+                    col = 0
+                    line += 1
+                else:
+                    col += 1
+                    if c != " ":
+                        found_non_empty_chars = True
+
+                # if we've only seen newlines and whitespace, keep incrementing selection start
+                # this way the highlighted portion actually starts at the code
+                if (c == " " or c == "\n") and not found_non_empty_chars:
+                    selection_start = (line, col)
+
+            selection_end = (line, col)
+
+            if found_query:
+                break
+
+        query = queries[query_idx_to_run] if query_idx_to_run != -1 else ""
+
+        return query, Selection(selection_start, selection_end)
+
+    def action_execute_query(self):
+        """
+        Executes the query at the cursor
+        """
+        cursor_x, cursor_y = self.text_area.cursor_location
+        query, selection = self._find_query_at_cursor(cursor_x, cursor_y)
+
+        if query.strip() == "":
+            self._display_error_in_table("no query to run")
+            return
+
+        self.text_area.selection = selection
+
         result = None
         cur = self.conn.cursor()
 
-        def display_error_in_table(error: Exception):
-            self.result_table.clear(columns=True)
-            self.result_table.add_column("error")
-            self.result_table.add_row(error)
-
-        for query in queries:
-            if query.strip() != "":
-                try:
-                    cur.execute(query)
-                    result = cur.fetchall()
-                except Exception as e:
-                    display_error_in_table(e)
-                    cur.close()
-                    return
+        try:
+            cur.execute(query)
+            result = cur.fetchall()
+        except Exception as e:
+            self._display_error_in_table(str(e))
+            cur.close()
+            return
 
         try:
             col_names = [col[0] for col in cur.description]
@@ -87,6 +161,6 @@ class DuckUI(App):
             self.result_table.add_columns(*col_names)
             self.result_table.add_rows(result)
         except Exception as e:
-            display_error_in_table(e)
+            self._display_error_in_table(str(e))
         finally:
             cur.close()
